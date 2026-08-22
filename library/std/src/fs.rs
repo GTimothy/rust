ba229aeec9cc31fa52b51c64f1de53d14c270ee5
GTimothy <22472919+GTimothy@users.noreply.github.com>
2026-08-22 14:56:33 +0200

    fs: optimize by directly checking path string for empty instead of allocating, remove reordering

--- library/std/src/fs.rs
+++ library/std/src/fs.rs
@@ -3590,7 +3590,7 @@ impl DirBuilder {
     fn create_dir_all(&self, path: &Path) -> io::Result<()> {
         // if path's parent is None, it is "/" path, which should
         // return Ok immediately
-        if path.parent() == None || path == Path::new("") {
+        if path.is_empty() || path.parent() == None {
             return Ok(());
         }
 
@@ -3601,7 +3601,7 @@ impl DirBuilder {
             // for relative paths like "foo/bar", the parent of
             // "foo" will be "" which there's no need to invoke
             // a mkdir syscall on
-            if ancestor.parent() == None || ancestor == Path::new("") {
+            if ancestor.is_empty() || ancestor.parent() == None {
                 break;
             }
 
